c0e412ad639e2c04c8b00cac263a624a64c6006f
vanntoengcaptaincambodia <toeng.vann@institute.pse.ngo>
2024-05-06 10:32:05 +0700

    nothing

--- js/deleteEmailFromLocal.js
+++ js/deleteEmailFromLocal.js
@@ -1,7 +1,6 @@
 function deleteEmailLogin() {
-    // Check if email login exists in local storage
     if (localStorage.getItem('email')) {
-        // Remove the email login from local storage
+        
         localStorage.removeItem('email');
         console.log('Email login deleted from local storage.');
     } else {
